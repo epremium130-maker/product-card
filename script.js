a0507f6786b4600a94e7d38e547ffe5f7bf6c943
EZ <epremium130@gmail.com>
2026-03-30 19:25:40 +0800

    fix: use textContent and add semicolons

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // Покраска первой карточки
 
-const recolorFirstCardButton = document.getElementById('recolor-first-card-button')
-const firstProductCard = document.querySelector('.product-card')
-const purpleHashColor = '#d4b1ff'
+const recolorFirstCardButton = document.getElementById('recolor-first-card-button');
+const firstProductCard = document.querySelector('.product-card');
+const purpleHashColor = '#d4b1ff';
 
 recolorFirstCardButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = purpleHashColor;
-})
+});
 
 // Покраска всех карточек
 
-const productCards = document.querySelectorAll('.product-card')
-const recolorProductCards = document.getElementById('recolor-product-cards-button')
-const orangeHashColor = '#ed8106c2'
+const productCards = document.querySelectorAll('.product-card');
+const recolorProductCards = document.getElementById('recolor-product-cards-button');
+const orangeHashColor = '#ed8106c2';
 
-recolorProductCards.addEventListener('click', () =>{
+recolorProductCards.addEventListener('click', () => {
   productCards.forEach(
     card => card.style.backgroundColor = orangeHashColor
   )
-})
+});
 
 // Переход на страницу Google
 
-const googleOpenButton = document.getElementById('open-google-button')
+const googleOpenButton = document.getElementById('open-google-button');
 googleOpenButton.addEventListener ('click', openGoogle);
 function openGoogle() {
-  const answer = confirm('Вы действительно хотите перейти на сайт Google?')
+  const answer = confirm('Вы действительно хотите перейти на сайт Google?');
 
   if (answer === true) {
     window.open('https://google.com')
@@ -34,21 +34,18 @@ function openGoogle() {
   }
 }
 
-// 
+// Функция выведения текста в консоль при наведении на заголовок
 
-const logHeaderTextContent = document.querySelector ('.main-header')
+const logHeaderTextContent = document.querySelector ('.main-header');
 
-logHeaderTextContent.addEventListener('mouseover', () =>{
-  console.log('Мышь в зоне заголовка')
-})
+logHeaderTextContent.addEventListener('mouseover', () => {
+  console.log(logHeaderTextContent.textContent)
+});
 
 // Кнопка изменяющая цвет
 
 const colorToggleButton = document.querySelector('.color-toggle-button');
 
 colorToggleButton.addEventListener('click',() => {
-
   colorToggleButton.classList.toggle('changed');
-
 });
-
