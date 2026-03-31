143a64a273fcef0b75c1492f0e2db381c6da031e
EZ <epremium130@gmail.com>
2026-03-31 14:34:25 +0800

    fix: add semicolons and space

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ const orangeHashColor = '#ed8106c2';
 
 recolorProductCards.addEventListener('click', () => {
   productCards.forEach(
-    card => card.style.backgroundColor = orangeHashColor
-  )
+    card => card.style.backgroundColor = orangeHashColor);
 });
 
 // Переход на страницу Google
@@ -28,9 +27,9 @@ function openGoogle() {
   const answer = confirm('Вы действительно хотите перейти на сайт Google?');
 
   if (answer === true) {
-    window.open('https://google.com')
+    window.open('https://google.com');
   } else {
-    console.log('Пользователь отменил переход на Google')
+    console.log('Пользователь отменил переход на Google');
   }
 }
 
@@ -39,13 +38,13 @@ function openGoogle() {
 const logHeaderTextContent = document.querySelector ('.main-header');
 
 logHeaderTextContent.addEventListener('mouseover', () => {
-  console.log(logHeaderTextContent.textContent)
+  console.log(logHeaderTextContent.textContent);
 });
 
 // Кнопка изменяющая цвет
 
 const colorToggleButton = document.querySelector('.color-toggle-button');
 
-colorToggleButton.addEventListener('click',() => {
+colorToggleButton.addEventListener('click', () => {
   colorToggleButton.classList.toggle('changed');
 });
