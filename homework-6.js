b846abb81e8cadb2e77e4c1ee37f13e8baee95d7
EZ <258454598+epremium130-maker@users.noreply.github.com>
2026-05-14 12:08:17 +0800

    created arrays, object and functions. also used .map

---
+++ homework-6.js
@@ -0,0 +1,22 @@
+const personalInfo = {
+  name: "Eziz",
+  age: 23,
+  country: "China",
+  city: "Wuhan",
+  status: "student",
+  major: "Teaching Chinese",
+  course: 1,
+  featureJob: "frontend developer"
+}
+
+console.log(personalInfo)
+
+const car = {
+  name: "BMW",
+  model: "m3",
+  year: 2025,
+  color: "black",
+  gearbox: "automat"
+}
+
+console.log(car)
