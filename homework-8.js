d026f8d2edf761bcc57e89cbe878f4105fff7b7d
EZ <258454598+epremium130-maker@users.noreply.github.com>
2026-06-24 09:34:52 +0800

    used prompt and rendering fuction with product cards array

---
+++ homework-8.js
@@ -0,0 +1,61 @@
+// Импорт массива с другого файла 
+
+import { productCards } from "./product-cards.js";
+
+const cardTemplate = document.querySelector("#product-card-template")
+const cardWrapper = document.querySelector(".product-card__wraper")
+
+// Функция-конвейер для отрисовки карточек
+
+function renderCards(productCards, count) {
+
+cardWrapper.innerHTML = '';
+
+for (let i = 0; i < count; i++) {
+
+const cardClone = cardTemplate.content.cloneNode(true);
+
+const currentProduct = productCards[i];
+
+const titleElement = cardClone.querySelector('.product-card__title');
+titleElement.textContent = currentProduct.title;
+
+const descrElement = cardClone.querySelector('.product-card__descr');
+descrElement.textContent = currentProduct.descr;
+
+const priceElement = cardClone.querySelector('.price-value');
+priceElement.textContent = currentProduct.price;
+
+const imgElement = cardClone.querySelector('.product-card__img img');
+imgElement.src = currentProduct.img;
+imgElement.alt = currentProduct.title;
+
+// Выводим даннык состава в li-шки
+
+const compoundList = cardClone.querySelector('.product-card__compound');
+
+currentProduct.compound.forEach(function(ingredient) {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  compoundList.append(li);
+});
+
+cardWrapper.append(cardClone);
+}
+}
+
+// Функция инициализации (старт программы) и защита ввода
+
+function init() {
+  const count = +prompt('Сколько карточек отобразить от 1 до 5?');
+
+
+if (count < 1 || count > 5 || isNaN(count)) {
+  alert('Неверное число! Пожалуйста, введите цифру от 1 до 5.');
+  return;
+}
+
+renderCards(productCards, count);
+}
+
+init();
